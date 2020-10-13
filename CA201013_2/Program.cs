ba9413de6cada3644a3c813801950cc95ba96208
Juhasz Zoltan <juhaszzoltanmate@yahoo.com>
2020-10-13 12:07:57 +0200

    Add project files.

---
+++ CA201013_2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA201013_2
+{
+    class DiakC
+    {
+        public string nev;
+        public int kor;
+    }
+    struct DiakS
+    {
+        public string nev;
+        public int kor;
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var d1 = new DiakC { nev = "Osztály Dezső", kor = 18, };
+            var d2 = new DiakS { nev = "Struktúra István", kor = 12, };
+            var referenciaD1 = d1;
+            var masolatD2 = d2;
+            referenciaD1.nev = "Géza";
+            masolatD2.nev = "Imre";
+            Console.WriteLine($"eredeti: {d1.nev}");
+            Console.WriteLine($"eredeti: {d2.nev}");
+            Console.WriteLine($"másolat: {referenciaD1.nev}");
+            Console.WriteLine($"másolat: {masolatD2.nev}");
+            Console.ReadKey();
+        }
+    }
+}
